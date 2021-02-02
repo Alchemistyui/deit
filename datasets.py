4d41836702c5676ed6b8dfbe5576cd293af9f1df
Alchemist <1319525622@qq.com>
2021-02-02 15:41:46 +0900

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -56,8 +56,11 @@ class INatDataset(ImageFolder):
 def build_dataset(is_train, args):
     transform = build_transform(is_train, args)
 
-    if args.data_set == 'CIFAR':
-        dataset = datasets.CIFAR100(args.data_path, train=is_train, transform=transform)
+    if args.data_set == 'CIFAR10':
+        dataset = datasets.CIFAR10(args.data_path, train=is_train, transform=transform)
+        nb_classes = 100
+    elif args.data_set == 'CIFAR100':
+        dataset = datasets.CIFAR100(args.data_path, train=is_train, transform=transform, download=True)
         nb_classes = 100
     elif args.data_set == 'IMNET':
         root = os.path.join(args.data_path, 'train' if is_train else 'val')
